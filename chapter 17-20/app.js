137d427e61fba85639d2fd5ee02b3e858ecee088
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-08-19 03:24:12 -0700

    chap 17-19 completed

--- chapter 17-20/app.js
+++ chapter 17-20/app.js
@@ -1,3 +1,12 @@
+// question no 1
+
+var multiArr = [
+
+    ["faizan","Rehman","Hasnain"],[1,2,3],[4,5,6]
+]
+
+console.log(multiArr[1]);
+
 
 
 // for (var i=1; i<=10; i++){
@@ -41,57 +50,97 @@
 
 // even counting
 
-for(i=0 ; i<=20; i++){
+// for(i=0 ; i<=20; i++){
  
-if(i%2 == 0){
-    console.log(i);
+// if(i%2 == 0){
+//     console.log(i);
     
-}
+// }
 
-}
+// }
 
 // odd counting
 
 
-for(i=0; i<=20 ; i++){
+// for(i=0; i<=20 ; i++){
 
-    if(i%2 != 0){
-        console.log(i);
-    }
-}
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+// }
 
-for(i=2; i<=20; i+=2){
+// for(i=2; i<=20; i+=2){
 
-    console.log(i+"K")
-}
+//     console.log(i+"K")
+// }
 
 
 // reverse
 
-for(i=20; i>=1 ; i--){
+// for(i=20; i>=1 ; i--){
 
-    console.log(i)
-}
+//     console.log(i)
+// }
 
 // question no7
 
-var arr = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var arr = ["cake", "apple pie", "cookie", "chips", "patties"];
 
-function found(){
+// function found(){
 
-var user = document.getElementById("userInput").value
+// var user = document.getElementById("userInput").value
 
-for(i = 0; i<=arr.length-1; i++){
-if(user.toLowerCase() == arr[i].toLowerCase()){
-    console.log("element found at index",i);
-}
-else{
-    console.log("Element not found")
-}
+// for(i = 0; i<=arr.length-1; i++){
+// if(user.toLowerCase() == arr[i].toLowerCase()){
+//     console.log("element found at index",i);
+// }
+// else{
+//     console.log("Element not found")
+// }
 
-}
+// }
 
 
-}
+// }
+
+
+
+// question no 8
+
+// var arr = [100,5,70,19,27,3,45,60,91];
+// var Max = arr[0];
+// for(i=0; i<arr.length; i++)
+// {
+
+//     if(arr[i] > Max){
+//         Max = arr[i];
+
+//     }
+
+// }
+// console.log("The maximum number is: "+Max);
+
+
+// question no 9
+
+// var arr = [-2,2,1,4,0,-1,5,6];
+// var smallest = arr[0];
+
+// for(i = 0; i< arr.length; i++){
+
+//     if(arr[i] < smallest){
+//         smallest = arr[i];
+//     }
+
+// }
+
+// console.log(smallest);
+
+// question no 10
+
+// for(i = 5; i<=100; i+=5){
+
+//     console.log(i);
+// }
 
 
