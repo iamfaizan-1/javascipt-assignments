ffea8e16e395a5496b1870f378917061fe30b0f1
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-08-19 03:02:07 -0700

    chapter 17

---
+++ chapter 17-20/app.js
@@ -0,0 +1,97 @@
+
+
+// for (var i=1; i<=10; i++){
+
+// console.log(i);
+
+// }
+
+// question no 4
+
+// var table = prompt("Enter multiplication table");
+// var length = prompt("Enter the length of the table");
+
+// for(i = 1; i<=length; i++){
+
+// var result = table*i;
+// console.log(table + "x" + i + "="+result);
+
+// }
+
+// question no 5
+
+// var fruit = ["apple","banana","mango","orange","strawberry"];
+
+// for(i = 0; i<= fruit.length-1; i++){
+// console.log("Element at index"+ i +" is " +fruit[i]);
+
+// }
+
+
+// question no 6
+
+// a counting
+
+// for(i = 1; i <= 15; i++){
+//     document.write(i + "  ");
+// }
+// document.write("</br>"); 
+// reverse counting
+
+
+// even counting
+
+for(i=0 ; i<=20; i++){
+ 
+if(i%2 == 0){
+    console.log(i);
+    
+}
+
+}
+
+// odd counting
+
+
+for(i=0; i<=20 ; i++){
+
+    if(i%2 != 0){
+        console.log(i);
+    }
+}
+
+for(i=2; i<=20; i+=2){
+
+    console.log(i+"K")
+}
+
+
+// reverse
+
+for(i=20; i>=1 ; i--){
+
+    console.log(i)
+}
+
+// question no7
+
+var arr = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+function found(){
+
+var user = document.getElementById("userInput").value
+
+for(i = 0; i<=arr.length-1; i++){
+if(user.toLowerCase() == arr[i].toLowerCase()){
+    console.log("element found at index",i);
+}
+else{
+    console.log("Element not found")
+}
+
+}
+
+
+}
+
+
