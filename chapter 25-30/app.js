9d9660a6ee48121ad3135eb389d2eb4b4ac3de73
iamfaizan-1 <faizanfaizy634@gmail.com>
2025-09-19 23:54:59 -0700

    practice

---
+++ chapter 25-30/app.js
@@ -0,0 +1,52 @@
+// var input = +prompt("Enter a positive integer");
+
+// var choice = prompt("Choose an option \n" + "a. Show number \n" + "b. Round off value \n"+"c. Floor value \n" +"d. Ceil value ");
+
+// if(choice === "a"){
+
+//     document.write("Number"+input);
+// }
+
+// else if (choice === "b"){
+//     document.write("Round off number:"+Math.round(input))
+// }
+
+// else if (choice === "c"){
+//     document.write("Floor value:"+Math.floor(input))
+// }
+// else if (choice === "d"){
+//     document.write("Ceil value:"+Math.ceil(input))
+// }
+
+// else{
+
+//     document.write("Invalid option")
+// }
+
+
+// question no 2
+
+// var num = +prompt("Enter a negative floating number");
+
+// document.write("Number:"+ Number(num) + "</br>")
+// document.write("Round off:"+ Math.round(num) + "</br>" )
+// document.write("Floor :"+ Math.floor(num) + "</br>")
+// document.write("Ceil:"+ Math.ceil(num) )
+
+
+
+// question no 3
+
+// var num = +prompt("Enter number");
+
+
+// if(num >= 0){
+//     document.write("Absolute value of number is:" + num);
+// }
+
+// else{
+//     document.write("Absolute value of numb is:"+ num *-1)
+// }
+
+
+
---
+++ todolist/app.js
@@ -0,0 +1,65 @@
+// function toggleBulb() {
+//   var image = document.getElementById("image");
+//   var btn = document.getElementById("btn");
+
+//   if (image.src.includes("/assets/bulbOff.jpg")) {
+//     image.src = "./assets/bulbOn.jpg";
+//     btn.innerText = "Off"
+//   } else {
+//     image.src = "./assets/bulbOff.jpg"
+//     btn.innerText = "On"
+//   }
+// }
+
+// var image = document.getElementById('image');
+// function imageFun() {
+//   image.className = "imageAnimate"
+// }
+
+// function changeAnimation() {
+//   image.className += "changeAnimate"
+// }
+
+// var images = document.getElementsByTagName("img");
+
+// console.log("images ==>", images);
+
+
+// var rules = document.getElementById("rules");
+// var paras = rules.getElementsByTagName("p");
+
+// for (var para of paras) {
+//   console.log("para ==>", para.innerText);
+// }
+
+// console.log("check ==>", document.childNodes[1].childNodes);
+
+
+// var para = document.getElementById("para")
+
+var input = document.getElementById("input");
+
+function addTodo() {
+  var ul = document.getElementById("output");
+  var li = document.createElement("li");
+  var counter = ul.childElementCount;
+
+  li.innerHTML = "<div class='outputLi'>" + input.value + "<button onclick='deleteTodo(" + counter + ")'>Delete</button></div>"
+  li.setAttribute("id", counter)
+  ul.appendChild(li)
+  input.value = ""
+}
+
+function deleteTodo(id) {
+  var li = document.getElementById(id)
+  li.remove();
+}
+
+// edit button, with id pass
+// editBtn, get li with id
+// get text of that li, and set the input value with the li text
+// hide the add button, show the edit btn,
+// add a function in the attribue onclick in edit btn
+// make a function editTodo
+// get the same li, set the value with new value;
+// hide the edit btn, show the add btn.
